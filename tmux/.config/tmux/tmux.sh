7db16e6c73ced29ab43f3041f0faf8b492253e97
Alex <alex@alexhe.io>
2021-08-02 09:39:26 +1000

    fix(tmux): use printf to satisfy shellcheck (SC2028)

--- tmux/.config/tmux/tmux.sh
+++ tmux/.config/tmux/tmux.sh
@@ -3,7 +3,7 @@
 
 # Installs Tmux Plugin Manager
 echo "Installing Tmux Plugin Manager..."
-echo "\n"
+printf "\n"
 
 if ! command -v tmux >/dev/null 2>&1; then
   echo "\u274c: The command 'tmux' was not found on your system."
@@ -11,7 +11,7 @@ if ! command -v tmux >/dev/null 2>&1; then
   echo "before attempting to install the plugin manager!"
 else
   git clone https://github.com/tmux-plugins/tpm "$XDG_CONFIG_HOME/tmux/plugins/tpm"
-  echo "\n"
+  printf "\n"
   echo "Tmux Plugin Manager has been installed."
   echo "Note: You can install plugins by specifying in tmux.conf"
 fi
