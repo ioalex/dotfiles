c6077b7e93bf5ba201771a94c3f38515481c516d
Alex <alex@alexhe.io>
2021-08-02 10:58:23 +1000

    fix(zsh): addresses shellcheck warnings and errors in zshrc

--- zsh/.config/zsh/.zsh/prompt.zsh
+++ zsh/.config/zsh/.zsh/prompt.zsh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # shellcheck shell=bash
+# shellcheck disable=SC1087,SC2154
 
 # Fall back Prompt
 # Enable colors and change prompt:
@@ -7,4 +8,5 @@ autoload -U colors && colors	# Load colors
 PS1="%B%{$fg[red]%}[%{$fg[yellow]%}%n%{$fg[green]%}@%{$fg[blue]%}%M %{$fg[magenta]%}%~%{$fg[red]%}]%{$reset_color%}$%b "
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+# shellcheck source=/dev/null
 [[ ! -f ~/.config/zsh/.p10k.zsh ]] || source ~/.config/zsh/.p10k.zsh
