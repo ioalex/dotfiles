17d3e87ea1de0875ece0d4d157e14555a8d60075
Alex <alex@alexhe.io>
2021-08-09 23:26:34 +1000

    feat(zsh/history): updated zsh options for history

--- zsh/.config/zsh/.zsh/history.zsh
+++ zsh/.config/zsh/.zsh/history.zsh
@@ -1,18 +1,24 @@
 # shellcheck shell=bash
 # shellcheck disable=SC2034
 
-# History Setup
+# History
+setopt BANG_HIST                          # Perform textual history expansion, csh-style, treating the character ‘!’ specially.
+setopt EXTENDED_HISTORY                   # Save each command’s beginning timestamp (in seconds since the epoch) and the duration (in seconds) to the history file.
+setopt HIST_FIND_NO_DUPS                  # Up Arrow / Down Arrow ignore duplicates.
+setopt HIST_IGNORE_ALL_DUPS               # Remove older duplicate entries from history.
+setopt HIST_IGNORE_DUPS                   # Do not enter command lines into the history list if they are duplicates of the previous event.
+setopt HIST_IGNORE_SPACE                  # Remove command lines from the history list when the first character on the line is a space.
+setopt HIST_REDUCE_BLANKS                 # Remove superfluous blanks from each command line being added to the history list.
+setopt HIST_SAVE_NO_DUPS                  # When writing out the history file, older commands that duplicate newer ones are omitted.
+setopt HIST_VERIFY                        # Don't execute the command directly upon history expansion.
+setopt INC_APPEND_HISTORY                 # zsh sessions append to the history, rather than replacing.
+setopt SHARE_HISTORY                      # Share history between different instances of the shell.
+
+# Variables
 HISTFILE="$ZSH_HISTORY_DIR"/history
 HISTSIZE=50000
 SAVEHIST=$HISTSIZE
 HISTTIMEFORMAT="%T: "
-
-setopt INC_APPEND_HISTORY # ZSH sessions append to the history, rather than replacing
-setopt HIST_REDUCE_BLANKS # Remove superfluous blanks from history items
-setopt HIST_FIND_NO_DUPS # Up Arrow / Down Arrow ignore duplicates
-setopt HIST_IGNORE_DUPS # Consecutive duplicates do not get added to the history
-setopt HIST_IGNORE_ALL_DUPS # Remove older duplicate entries from history
-setopt SHARE_HISTORY # Share history between different instances of the shell
 
 # Ignore interactive commands from history
 export HISTORY_IGNORE="(ls|pwd|exit|cd ..|clear|c)"
