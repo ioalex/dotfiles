26007081d5e7b42d2529cc20b460bf25ec54b47d
Alex <alex@alexhe.io>
2021-08-15 15:26:12 +1000

    chore(fzf.zsh): wrap fzf configuration in conditional

--- zsh/.config/zsh/.zsh/misc/fzf.zsh
+++ zsh/.config/zsh/.zsh/misc/fzf.zsh
@@ -3,12 +3,15 @@
 
 # shellcheck source=/dev/null
 # FZF Configuration
-[ -f ~/.config/fzf/fzf.zsh ] && source ~/.config/fzf/fzf.zsh
-export FZF_DEFAULT_OPS="--extended"
 
-if type rg &> /dev/null; then
-  export FZF_DEFAULT_COMMAND='rg --hidden -l ""'
+if [ -x "$(command -v fzf)" ]; then
+  [ -f ~/.config/fzf/fzf.zsh ] && source ~/.config/fzf/fzf.zsh
+  export FZF_DEFAULT_OPS="--extended"
+
+  if type rg &> /dev/null; then
+    export FZF_DEFAULT_COMMAND='rg --hidden -l ""'
+  fi
+
+  # export FZF_CTRL_T_COMMAND=$FZF_DEFAULT_COMMAND
+  export FZF_CTRL_T_COMMAND='find -L -H'
 fi
-
-# export FZF_CTRL_T_COMMAND=$FZF_DEFAULT_COMMAND
-export FZF_CTRL_T_COMMAND='find -L -H'
